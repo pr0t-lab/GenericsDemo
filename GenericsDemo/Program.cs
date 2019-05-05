5d5ecf81b126d038920db4ddd2e47600b58eaf0e
pr0t <prot@prot.com.pl>
2019-05-05 22:59:10 +0200

    Add new collections

--- GenericsDemo/Program.cs
+++ GenericsDemo/Program.cs
@@ -8,8 +8,20 @@ namespace GenericsDemo
 {
     class Program
     {
+        public static bool IsOdd(int value)
+        {
+            return value % 2 != 0;
+        }
+
         static void Main(string[] args)
         {
+            var list1 = new List<int> { 1, 2, 3, 4, 5 };
+            var list2 = new List<int> { 2, 4, 6, 8, 10 };
+            var set1 = new HashSet<int> { 3, 6, 9, 12, 15 };
+            /*var*/ int[] array1 = new int[5] { 4, 8, 12, 16, 20 };   // new[] { .. }; or new int[] { .. }
+            //int[] intArray = new int[2];
+            //intArray[0] = 1;
+            //intArray[1] = 2;
         }
     }
 }
