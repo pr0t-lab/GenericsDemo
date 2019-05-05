9cfc585d9b3b3de210e05744454b3b900aefe0bf
pr0t <prot@prot.com.pl>
2019-05-05 23:17:03 +0200

    Add new class: IEnumerableExtensions

---
+++ GenericsDemo/IEnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    public static class IEnumerableExtensions
+    {
+        public static int CountOdd(this IEnumerable<int> @this)
+        {
+            int numOdd = 0;
+
+            foreach(var element in @this)
+            {
+                if (element % 2 != 0)
+                    numOdd++;
+            }
+
+            return numOdd;
+        }
+    }
+}
--- GenericsDemo/Program.cs
+++ GenericsDemo/Program.cs
@@ -13,6 +13,18 @@ namespace GenericsDemo
             return value % 2 != 0;
         }
 
+        public static int CountOdd(IEnumerable<int> collection)
+        {
+            int numOdd = 0;
+            foreach (var element in collection)
+            {
+                if (IsOdd(element))
+                    numOdd++;
+            }
+
+            return numOdd;
+        }
+
         static void Main(string[] args)
         {
             var list1 = new List<int> { 1, 2, 3, 4, 5 };
@@ -22,6 +34,14 @@ namespace GenericsDemo
             //int[] intArray = new int[2];
             //intArray[0] = 1;
             //intArray[1] = 2;
+
+            Console.WriteLine(string.Format("Number of odds in {0}: {1}", nameof(list1), list1.CountOdd() /*CountOdd(list1)*/));
+            Console.WriteLine(string.Format("Number of odds in {0}: {1}", nameof(list2), CountOdd(list2)));
+            Console.WriteLine(string.Format("Number of odds in {0}: {1}", nameof(set1), CountOdd(set1)));
+            Console.WriteLine(string.Format("Number of odds in {0}: {1}", nameof(array1), CountOdd(array1)));
+
+            Console.ReadLine();
+
         }
     }
 }
