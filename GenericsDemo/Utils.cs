0918d49b13862811d7e59032620b0163d8a8a3d8
pr0t <prot@prot.com.pl>
2019-05-18 17:39:17 +0200

    Add Utils class

---
+++ GenericsDemo/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace GenericsDemo
+{
+    public static class Utils
+    {
+        public static IEnumerable<T> Take<T>(IEnumerable<T> source, int n)
+        {
+            return null;
+        }
+    }
+}
